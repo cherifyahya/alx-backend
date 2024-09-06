6bf9b567269d9486274b4aff9a4eba51b6d665e0
yahya <cherifyahya069@gmail.com>
2024-09-06 22:50:46 +0000

    solved

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Basic dictionary"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """caching system"""
+    def __init__(self):
+        """constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ get item from the cache by a key"""
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
